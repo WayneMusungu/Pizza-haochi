0fd107da5227ffbcc9f138164b2ff709197fa6ce
WayneMusungu <waynemuyera17@gmail.com>
2022-03-20 21:47:36 +0300

    Add function in scripts. js

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,8 +1,16 @@
 // EFFECTS ON THE HOME SECTION
-$(document).ready(function(){
-    $(".col-md-4").click(function(){
+$(document).ready(function () {
+    $(".col-md-4").click(function () {
         // create a variable speed for our slideToggle
         var speed = 2000
         $(this).children(".hide").slideToggle(speed)
     });
 });
+
+
+
+function pizzaCosts(crust, size, topping) {
+    this.crust = crust;
+    this.size = size;
+    this.toppings = topping;
+};
