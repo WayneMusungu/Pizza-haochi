12030087a0704c66d066f84f67cde672cfa62aa0
WayneMusungu <waynemuyera17@gmail.com>
2022-03-20 22:39:01 +0300

    add variables prices in rspect to their indexes

--- Js/scripts.js
+++ Js/scripts.js
@@ -14,3 +14,20 @@ function pizzaCosts(crust, size, topping) {
     this.size = size;
     this.toppings = topping;
 };
+
+function Cost(price, count, delivery) {
+    this.price = price;
+    this.count = count;
+    this.delivery = delivery;
+
+};
+Cost.prototype.initialCost = function () {
+    return this.price * this.count + this.delivery;
+};
+
+
+var pizzaPrices = [500, 750, 1250];
+var crustCost = [100, 150, 200];
+var toppingCost = [80, 60, 100, 120, 90];
+var deliveryCost = [300, 0];
+
